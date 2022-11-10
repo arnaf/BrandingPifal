8b79f7fbe56c9627be392d29effb3eccfd4fea88
Nivra <arvinaufal23@gmail.com>
2022-11-10 23:15:08 +0700

    push refresh naufal

---
+++ database/migrations/2022_11_10_145647_create_cashiers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cashiers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+
+            $table->text('photo');
+            $table->string('name');
+            $table->string('employeeId');
+            $table->string('dateBirth');
+            $table->string('phone');
+            $table->text('address');
+            $table->text('status');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cashiers');
+    }
+};
---
+++ resources/views/menus/cashier.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+<main id="main" class="main">
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <div class="col-sm-6">
+              <h1>Cashier</h1>
+            </div>
+
+          <div class="card-tools">
+
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+
+                  <button type="button" class="my-3 btn btn-primary" onclick="create()">Tambah Data Kasir</button>
+
+              <table class="table table-hover table-striped table-border" id="table">
+
+                  <thead>
+                      <th>#</th>
+                      <th>Foto</th>
+                      <th>Nama</th>
+                      <th>Nomor Pegawai</th>
+                      <th>Tanggal Lahir</th>
+                      <th>Alamat</th>
+                      <th>Telp</th>
+                      <th>Status</th>
+                      <th>Tindakan</th>
+                  </thead>
+
+                  <tbody>
+
+
+                  </tbody>
+              </table>
+          </div>
+        </div>
+      </div>
+      @include('menus.modals.cashiers.create')
+      @include('menus.modals.cashiers.edit')
+
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+    @push('script')
+      @include('menus.scripts.datatables')
+      @include('menus.scripts.sweetalert')
+      @include($script)
+    @endpush
+  </section>
+
+</main>
+@endsection
